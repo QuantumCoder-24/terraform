73d3f27dd2a998c24abce56815e6c6631beaf2b6
Abhinav Pathak <161750320+QuantumCoder-24@users.noreply.github.com>
2024-03-01 19:14:57 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "vpc_id" {}
+variable "myip" {}
+variable "env_prefix" {}
+variable "image_name" {}
+variable "instance-type" {}
+variable "avail_zone" {}
+variable "subnet_id" {}
+variable "public_key_location" {}
